1200661f59c5fb8410b41d21ccd607ca8853b77d
João Marco Silva <jotamarco@p186.glmf.di.uminho.pt>
2018-03-09 13:38:25 +0000

    Tipos de retorno atualizados

--- include/interface.h
+++ include/interface.h
@@ -8,7 +8,7 @@ typedef struct TCD_community * TAD_community;
 TAD_community init();
 
 // query 0
-void load(TAD_community com);  //diretoria onde estarão os ficheiros do dump
+TAD_community load(TAD_community com, char* dump_path);  //diretoria onde estarão os ficheiros do dump
 
 // query 1
 STR_pair info_from_post(TAD_community com, int id);  
@@ -17,7 +17,7 @@ STR_pair info_from_post(TAD_community com, int id);
 LONG_list top_most_active(TAD_community com, int N);
 
 // query 3
-//LONG_pair total_posts(TAD_community com, Date begin, Date end);
+LONG_pair total_posts(TAD_community com, Date begin, Date end);
 
 // query 4
 LONG_list questions_with_tag(TAD_community com, char* tag, Date begin, Date end);
@@ -41,6 +41,6 @@ LONG_list both_participated(TAD_community com, long id1, long id2, int N);
 LONG_list better_answer(TAD_community com, int id);
 
 // query 11
-//LONG_LIST most_used_best_rep(TAD_community com, int N, Date begin, Date end);
+LONG_list most_used_best_rep(TAD_community com, int N, Date begin, Date end);
 
 TAD_community clean(TAD_community com);
