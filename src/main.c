bef9dfbb6f0b4f4a62ab1c2f88a4d1523687cf12
Pedro Moreira <pedrombm19@gmail.com>
2018-03-14 23:07:29 +0000

    Debuging load()

--- src/main.c
+++ src/main.c
@@ -6,7 +6,6 @@
 #include <string.h>
 
 // teste: passar para um array de inteiros os primeiros 50 id's presentes em Users.xml
-// !!!!! prototipo load() alterado
 
 
 /*NOTAS:
@@ -48,16 +47,20 @@ struct TCD_community{
 
 TAD_community init(){
 	struct TCD_community* new = malloc(sizeof(struct TCD_community));
-  	GHashTable* newUserHash = g_hash_table_new (g_int_hash,g_int_equal);
-  	GHashTable* newPostHash = g_hash_table_new (g_int_hash,g_int_equal);
+  	GHashTable* newUserHash = g_hash_table_new(g_int_hash, g_int_equal);
+  	GHashTable* newPostHash = g_hash_table_new(g_int_hash, g_int_equal);
+
   	new->user = newUserHash;
   	new->post = newPostHash;
+
   	return new;
 }
 
 TAD_community load(TAD_community com, char* dump_path){
-	xmlChar* id; int *aux;
+	xmlChar* id; 
 	xmlChar* name;
+	int *aux = 0;
+	int i = 0;
 
 	xmlDocPtr doc = xmlParseFile(dump_path);
 	if(!doc){
@@ -69,43 +72,52 @@ TAD_community load(TAD_community com, char* dump_path){
 		printf("Empty Document\n");
 		xmlFreeDoc(doc);
 	}
-
 	else{		
 		cur = cur->xmlChildrenNode;
-		while(cur){
+		while(cur && i<1){
    			id = xmlGetProp(cur, (const xmlChar *)"Id");
    			name = xmlGetProp(cur, (const xmlChar *)"DisplayName");
-   			if(id!=NULL){
-   				sscanf((const char*)id, "%d", aux);
+   			if(id != NULL){
    				struct User* new = malloc(sizeof(struct User));
-   				new->id=*aux;
+   				
+   				sscanf((const char*)id, "%d", aux);
+   				new->id = *aux; 
    				strcpy(new->display_name,(const char*)name);
-   				g_hash_table_insert(com->user,aux,new);
+   				g_hash_table_insert(com->user, aux, new);
+   				printf("Teste: %d %s\n", new->id, new->display_name); // nao imprime
+				printf("teste\n"); // nao imprime
    			}
+   			printf("%d\n", (int)id); // id == 0 logo nao está a entrar no if
 			xmlFree(id);
 			xmlFree(name);
 			cur = cur->next;
+			i++;
 		}
 	}
 	xmlFreeDoc(doc);
+
 	return(com);		
 }
 
 
 /*
 STR_pair info_from_post(TAD_community com, int id){
-	// se tivermos uma hash é em tempo constante
-	int hash = hash(id);
-	while((com->info)[hash] != id && (com->estado)[i] != LIVRE) hash++;
-	if()
 
 }
 */
 
 
-
+void iterator(gpointer key, gpointer value, gpointer user_data){
+	printf(user_data, *(gint*)key, value);
+}
 
 int main(){
-  //struct TCD_community* teste = init();
-  return 0;
+	struct TCD_community* teste = init();
+	char* path = "../../dump exemplo/android/Users.xml";
+	
+	load(teste, path);
+	
+	g_hash_table_foreach(teste->user, (GHFunc)iterator, "%d");
+  
+  	return 0;
 } 
