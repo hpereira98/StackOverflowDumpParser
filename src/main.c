02a02d6b0b971171733d3348fd297a4ecea0fe3b
Pedro Moreira <pedrombm19@gmail.com>
2018-03-16 19:22:02 +0000

    Parser Posts.xml

--- src/main.c
+++ src/main.c
@@ -49,7 +49,7 @@ struct TCD_community{
 TAD_community init(){
 	struct TCD_community* new = malloc(sizeof(struct TCD_community));
   	GHashTable* newUserHash = g_hash_table_new(g_direct_hash, g_direct_equal);
-  	GHashTable* newPostHash = g_hash_table_new(g_int_hash, g_int_equal);
+  	GHashTable* newPostHash = g_hash_table_new(g_direct_hash, g_direct_equal);
 
   	new->user = newUserHash;
   	new->post = newPostHash;
@@ -60,33 +60,46 @@ TAD_community init(){
 TAD_community load(TAD_community com, char* dump_path){
 	xmlChar* id; 
 	xmlChar* name; 
-	int i=0;//so para comparar tamanho da hashT com numero insercoes
-;
+	xmlChar* titulo;
 
-	xmlDocPtr doc = xmlParseFile(dump_path);
-	if(!doc){
+	int i=0;//so para comparar tamanho da hashT com numero insercoes
+	char users[9] = "Users.xml";
+	char* users_path = malloc(sizeof(strlen(dump_path)+strlen(users)));
+	strcat(users_path, dump_path); strcat(users_path, users);
+
+	char posts[9] = "Posts.xml";
+	char* posts_path = malloc(sizeof(strlen(dump_path)+strlen(posts)));
+	strcat(posts_path, dump_path); strcat(posts_path, posts);
+
+
+	// USERS
+
+	xmlDocPtr doc_users = xmlParseFile(users_path);
+	if(!doc_users){
 		printf("Document not parsed successfully\n");
 	}
 
-	xmlNodePtr cur = xmlDocGetRootElement(doc);
+	xmlNodePtr cur = xmlDocGetRootElement(doc_users);
 	if(!cur){
 		printf("Empty Document\n");
-		xmlFreeDoc(doc);
+		xmlFreeDoc(doc_users);
 	}
 	else{		
 		cur = cur->xmlChildrenNode;
 		while(cur){
-   			id = xmlGetProp(cur, (const xmlChar *)"Id");
+   			id = xmlGetProp(cur, (const xmlChar *)"AccountId");
    			name = xmlGetProp(cur, (const xmlChar *)"DisplayName");
+
    			if(id != NULL){
-   				int* idUser=malloc(sizeof(int));
-   				struct User* new = g_new(struct User,1);//forma de fazer malloc
-   				new->display_name = malloc(strlen((const char*)name));
+   				int* idUser = malloc(sizeof(int));
+   				struct User* new = g_new(struct User, 1);//forma de fazer malloc
    				
-   				sscanf((const char*)id, "%d",idUser); 
-   				new->id = *idUser;			 
+   				new->display_name = malloc(strlen((const char*)name));
    				strcpy(new->display_name,(const char*)name);
 
+   				sscanf((const char*)id, "%d", idUser); 
+   				new->id = *idUser;			 
+   				
    				g_hash_table_insert(com->user, (gpointer*)idUser, new); i++;
    				
    				
@@ -97,33 +110,76 @@ TAD_community load(TAD_community com, char* dump_path){
 			xmlFree(id);
 			xmlFree(name);
 			cur = cur->next;
-		;
 		}
 	}
-	printf("Num insercoes: %d\n",i );
-	xmlFreeDoc(doc);
+	printf("Users: %d\n", i);
+	xmlFreeDoc(doc_users);
 
+	/*
+	// POSTS
+	i = 0;
+
+	xmlDocPtr doc_posts = xmlParseFile(posts_path);
+	if(!doc_posts){
+		printf("Document not parsed successfully\n");
+	}
+
+	printf("Abriu Posts.xml\n"); // Teste
+
+	cur = xmlDocGetRootElement(doc_posts);
+	if(!cur){
+		printf("Empty Document\n");
+		xmlFreeDoc(doc_posts);
+	}
+	else{		
+		cur = cur->xmlChildrenNode;
+		while(cur){
+   			id = xmlGetProp(cur, (const xmlChar *)"OwnerUserId");
+   			xmlChar* teste = xmlGetProp(cur, (const xmlChar *)"Title");
+   			if(teste) titulo = teste; else titulo = ""; 
+
+   			if(id != NULL){
+   				int* idUser = malloc(sizeof(int));
+   				struct Post* new = g_new(struct Post, 1);//forma de fazer malloc
+   				
+   				new->titulo = malloc(strlen((const char*)titulo));
+   				strcpy(new->titulo,(const char*)titulo);
+
+   				sscanf((const char*)id, "%d", idUser); 
+   				new->id = *idUser;	
+
+   				printf("A inserir %d\n", i); //teste
+   				g_hash_table_insert(com->post, (gpointer*)idUser, new); i++;
+				printf("Inserido\n");
+   			}
+			xmlFree(id);
+			xmlFree(titulo);
+			cur = cur->next;
+		}
+	}
+	printf("Posts: %d\n", i);
+	xmlFreeDoc(doc_posts);
+	*/
 	return(com);		
 }
 
 
-/*
-<<<<<<< HEAD
-STR_pair info_from_post(TAD_community com, int id){ // mudar pois é post id
-	struct User* user = (struct User*)g_hash_table_lookup(com->user, (gconstpointer)id);
-	struct Post* post = (struct Post*)g_hash_table_lookup(com->post, (gconstpointer)id);
 
+STR_pair info_from_post(TAD_community com, int id){ // mudar pois é post id
+	struct Post* post = (struct Post*)g_hash_table_lookup(com->post, GINT_TO_POINTER(id));
+	
 	char* titulo = post->titulo;
+	gint user_id = post->id;
+	
+	struct User* user = (struct User*)g_hash_table_lookup(com->user, GINT_TO_POINTER(user_id));
+
 	char* nome = user->display_name;
 
 	STR_pair new = create_str_pair(titulo, nome);
 
 	return new;
-=======
-STR_pair info_from_post(TAD_community com, int id){
->>>>>>> 2a5215b7a8fd70f17c6aadb31ca6611edef7e343
 }
-*/
+
 
 
 void iterator(gpointer key, gpointer value, gpointer user_data){
@@ -139,15 +195,16 @@ void print(gpointer data, gpointer user_data){
 
 int main(){
 	struct TCD_community* teste = init();
-	char* path = "../../dumpexemplo/android/Users.xml";
+	char* path = "../../dumpexemplo/android/";
 	
 	load(teste, path);
 	
-	//g_hash_table_foreach(teste->user,(GHFunc)iterator,"%d %d %s\n");//imprimir id e display_name
+	//g_hash_table_foreach(teste->user,(GHFunc)iterator,"%d %d %s\n"); // imprimir id e display_name
 
-	GList* new = g_hash_table_get_keys(teste->user);
-	g_list_foreach(new,print,NULL);
+	/*GList* new = g_hash_table_get_keys(teste->user);
+	g_list_foreach(new,print,NULL);*/
 
-   	printf("Tamanho hash: %d\n",g_hash_table_size(teste->user));
+   	//printf("Tamanho hash: %d\n",g_hash_table_size(teste->user));
+  	
   	return 0;
 } 
