2a5215b7a8fd70f17c6aadb31ca6611edef7e343
PedroFerr101 <pferreira59@gmail.com>
2018-03-16 02:07:16 +0000

    insercao THash corrigida

--- src/main.c
+++ src/main.c
@@ -48,7 +48,7 @@ struct TCD_community{
 
 TAD_community init(){
 	struct TCD_community* new = malloc(sizeof(struct TCD_community));
-  	GHashTable* newUserHash = g_hash_table_new(g_int_hash, g_int_equal);
+  	GHashTable* newUserHash = g_hash_table_new(g_direct_hash, g_direct_equal);
   	GHashTable* newPostHash = g_hash_table_new(g_int_hash, g_int_equal);
 
   	new->user = newUserHash;
@@ -59,8 +59,8 @@ TAD_community init(){
 
 TAD_community load(TAD_community com, char* dump_path){
 	xmlChar* id; 
-	xmlChar* name;
-	int aux = 0; int i=0;//so para comparar tamanho da hashT com numero insercoes
+	xmlChar* name; 
+	int i=0;//so para comparar tamanho da hashT com numero insercoes
 ;
 
 	xmlDocPtr doc = xmlParseFile(dump_path);
@@ -79,13 +79,17 @@ TAD_community load(TAD_community com, char* dump_path){
    			id = xmlGetProp(cur, (const xmlChar *)"Id");
    			name = xmlGetProp(cur, (const xmlChar *)"DisplayName");
    			if(id != NULL){
+   				int* idUser=malloc(sizeof(int));
    				struct User* new = g_new(struct User,1);//forma de fazer malloc
    				new->display_name = malloc(strlen((const char*)name));
    				
-   				sscanf((const char*)id, "%d", &aux); 
-   				new->id = aux; 
+   				sscanf((const char*)id, "%d",idUser); 
+   				new->id = *idUser;			 
    				strcpy(new->display_name,(const char*)name);
-   				g_hash_table_insert(com->user,&aux, new); i++;
+
+   				g_hash_table_insert(com->user, (gpointer*)idUser, new); i++;
+   				
+   				
 
    				//printf("Teste: %d %s\n", new->id, new->display_name); 
 				
@@ -105,14 +109,14 @@ TAD_community load(TAD_community com, char* dump_path){
 
 /*
 STR_pair info_from_post(TAD_community com, int id){
-
 }
 */
 
 
 void iterator(gpointer key, gpointer value, gpointer user_data){
 	struct User* aux = (struct User*)value;
-	printf("%d %s\n", aux->id, aux->display_name);
+	int* keyId = (int* )key;
+	printf(user_data,*keyId, aux->id, aux->display_name);
 }
 
 void print(gpointer data, gpointer user_data){
@@ -126,10 +130,10 @@ int main(){
 	
 	load(teste, path);
 	
-	g_hash_table_foreach(teste->user,iterator,"");//imprimir id e display_name
+	//g_hash_table_foreach(teste->user,(GHFunc)iterator,"%d %d %s\n");//imprimir id e display_name
 
-	GList* new = g_hash_table_get_keys(teste->user);//algo esta mal, mete as chaves todas a 1
-	g_list_foreach (new,print,"");
+	GList* new = g_hash_table_get_keys(teste->user);
+	g_list_foreach(new,print,NULL);
 
    	printf("Tamanho hash: %d\n",g_hash_table_size(teste->user));
   	return 0;
