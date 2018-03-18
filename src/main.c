1076512e429d8417d2ef778e82a8191c15fd26c2
Pedro Moreira <pedrombm19@gmail.com>
2018-03-18 02:15:24 +0000

    Parser Posts.xml

--- src/main.c
+++ src/main.c
@@ -109,7 +109,73 @@ TAD_community load(TAD_community com, char* dump_path){
 	printf("Users: %d\n", i);
 	xmlFreeDoc(doc_users);
 
-	
+	// POSTS
+	i = 0;
+
+	xmlDocPtr doc_posts = xmlParseFile(posts_path);
+	if(!doc_posts){
+		printf("Document not parsed successfully\n");
+	}
+
+	/*Debugging*/ printf("Abriu Posts.xml\n");
+
+	cur = xmlDocGetRootElement(doc_posts);
+	if(!cur){
+		printf("Empty Document\n");
+		xmlFreeDoc(doc_posts);
+	}
+	else{		
+		cur = cur->xmlChildrenNode;
+		while(cur){
+			xmlChar* post_type_id = xmlGetProp(cur, (const xmlChar *)"PostTypeId");
+
+			/*Debugging*/ printf("Verificou o PostTypeId = %s\n", (char*)post_type_id); // está a dar null e para o programa. Porque??
+			
+			if(post_type_id!=NULL && strcmp((char*)post_type_id, "1") == 0){
+
+				/*Debugging*/ printf("Comparou o PostTypeId\n");
+	   			
+	   			xmlChar* post_id = xmlGetProp(cur, (const xmlChar *)"Id");printf("1\n");
+	   			xmlChar* user_id = xmlGetProp(cur, (const xmlChar *)"OwnerUserId");printf("2\n");
+	   			xmlChar* titulo = xmlGetProp(cur, (const xmlChar *)"Title");printf("3\n");
+
+	   			if(post_id != NULL){printf("4\n");
+	   				int* idOwner = malloc(sizeof(int));printf("4\n");
+	   				int* idPost = malloc(sizeof(int));printf("5\n");
+	   				struct Post* new = g_new(struct Post, 1);printf("6\n");
+	   				
+	   				// Titulo
+	   				new->titulo = malloc(strlen((const char*)titulo));printf("7\n");
+	   				strcpy(new->titulo,(const char*)titulo);printf("8\n");
+
+	   				// Owner ID
+	   				printf("Ola\n");if(user_id)sscanf((const char*)user_id, "%d", idOwner);else(printf("nao tem owner(?)\n")); printf("9\n");
+	   				new->owner_id = *idOwner;printf("10\n");
+
+	   				// Post ID
+					sscanf((const char*)post_id, "%d", idPost); 
+	   				new->id = *idPost;
+
+	   				/*Debugging*/ printf("A inserir %d\n", i);
+
+	   				// Inserir conforme o Post ID
+	   				g_hash_table_insert(com->post, (gpointer*)idPost, new);
+	   				
+	   				/*Debugging*/ printf("Inserido %d\n", i);
+	   				i++;
+
+	   			}
+				xmlFree(post_id);
+				xmlFree(user_id);
+				xmlFree(titulo);
+			}
+			/*Debugging*/ else printf("Verificou o PostTypeId e não é do tipo 1\n");
+			xmlFree(post_type_id);
+			cur = cur->next;
+		}
+	}
+	printf("Posts: %d\n", i);
+	xmlFreeDoc(doc_posts);
 	
 	return(com);		
 }
