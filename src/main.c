23d1074b56c8d40f4bcafb2939a311c03d2eac24
PedroFerr101 <pferreira59@gmail.com>
2018-03-24 15:55:54 +0000

    query 3

--- src/main.c
+++ src/main.c
@@ -241,9 +241,43 @@ STR_pair info_from_post(TAD_community com, int id){
 		else new = create_str_pair(post->titulo, post->owner_display_name);
 	}
 
-	printf("Data: %d-%d-%d", get_year(post->data), get_month(post->data), get_day(post->data));
+	
 
 	return new;
+}
+
+// QUERY 3
+
+void posts_count(gpointer key, gpointer post_pointer, gpointer info){
+	
+	struct Post* post = (struct Post*) post_pointer;
+	Date post_date = post->data;
+
+	Date begin = ((Date*)(info))[0];
+	Date end = ((Date*)(info))[1];
+
+	int numQuestions = ((int**)(info))[2];
+	int numAnswers = ((int**)(info))[3];
+	
+	if(comparaDatas(post_date,begin)==1 && comparaDatas(end,post_date)==1){
+		if(post->type_id==1) (numQuestions)++;
+			else (numAnswers)++;
+	}
+}
+
+
+LONG_pair total_posts(TAD_community com, Date begin, Date end){
+
+	int *numQuestions = malloc(sizeof(int));
+	int *numAnswers = malloc(sizeof(int));
+	*numQuestions = *numAnswers = 0;
+
+	void* info[4] = {begin, end, numQuestions, numAnswers};
+	
+	g_hash_table_foreach(com->post,posts_count,info);
+	LONG_pair totalPost= create_long_pair(*numQuestions,*numAnswers);
+
+	return totalPost;
 }
 
 
@@ -335,8 +369,13 @@ void testeAcessoUserHT(TAD_community com, int id){
 int main(){
 	struct TCD_community* teste = init();
 	char* path = "../../dumpexemplo/android/";
-	
+	Date inicio = createDate(12,9,2010);
+	Date fim = createDate(14,9,2010);
 	load(teste, path);
+	LONG_pair new1 = total_posts(teste,inicio,fim);
+	printf("%d %d\n",get_fst_long(new1),get_snd_long(new1));
+
+
 /* Funcao para Debugging de UserHashT*/
 	//g_hash_table_foreach(teste->user,(GHFunc)printUserHT,"%d %d %s\n");
 	//testeAcessoUserHT(teste,4980640);
