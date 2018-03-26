4968765fac6aec5a3e48efb6fd51f24efad5f8c8
Pedro Moreira <pedrombm19@gmail.com>
2018-03-26 03:09:40 +0100

    Query 7 - protótipo

--- src/main.c
+++ src/main.c
@@ -467,11 +467,10 @@ void insereId(int* v, int x, int i, int n){
 	for(n=n-1; n>i; n--){
 		v[n] = v[n-1];
 	}
-
 	v[i] = x;
 }
 
-void func(gpointer key_pointer, gpointer post_pointer, gpointer info){
+void ordenaScores(gpointer key_pointer, gpointer post_pointer, gpointer info){ // if(post->type_id != 1) {toda a função}
 	struct Post* post = (struct Post*) post_pointer;
 	int* ids = ((int**)info)[0];
 	Date begin = ((Date*)info)[1];
@@ -506,13 +505,60 @@ LONG_list most_voted_answers(TAD_community com, int N, Date begin, Date end){
 
 	void* info[6] = {(void*)ids, (void*)begin, (void*)end, (void*)ocupados, (void*)score, (void*)&N};
 
-	g_hash_table_foreach(com->post, func, info);
+	g_hash_table_foreach(com->post, ordenaScores, info);
 
 	LONG_list r = create_list(N);	
 	for(i = 0; i<N; i++) set_list(r, i, ids[i]);
 
 	return r;
 }
+
+
+
+// Query 7
+
+void ordenaNRespostas(gpointer key_pointer, gpointer post_pointer, gpointer info){ // if(post->type_id == 1) {toda a função}
+	struct Post* post = (struct Post*) post_pointer;
+	int* ids = ((int**)info)[0];
+	Date begin = ((Date*)info)[1];
+	Date end = ((Date*)info)[2];
+	int *ocupados = ((int**)info)[3];
+	int* n_respostas = ((int**)info)[4];
+	int *size = ((int**)info)[5];
+
+	if(comparaDatas(begin, post->data) == -1 && comparaDatas(post->data, end) == -1){
+		if( (*ocupados+1 != *size) || (*ocupados+1 == *size && post->score > score[*ocupados]) ){ // neste momento já sei que vai ser inserido
+			int pos = insert(n_respostas, post->n_respostas, *size);
+			if(pos != -1){
+				insereId(ids, post->id, pos, *size);
+				if(*ocupados < *size - 1) (*ocupados)++;
+			}
+		}
+	}
+
+}
+
+LONG_list most_answered_questions(TAD_community com, int N, Date begin, Date end){
+	int* ids = malloc(sizeof(int)*N);
+	int* n_respostas = malloc(sizeof(int)*N);
+	int *ocupados = 0;
+	int i;
+
+	for(i = 0; i<N; i++){
+		ids[i] = 0;
+		respostas[i] = 0;
+	}
+
+	void* info[6] = {(void*)ids, (void*)begin, (void*)end, (void*)ocupados, (void*)n_respostas, (void*)&N};
+
+	g_hash_table_foreach(com->post, ordenaNRespostas, info);
+
+	LONG_list r = create_list(N);	
+	for(i = 0; i<N; i++) set_list(r, i, ids[i]);
+
+	return r;
+}
+
 
 
 /* Funcao para Debugging de PostHashT */
