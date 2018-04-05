8ccf7be54019908ce2c04d311b3fe2d774227b23
PedroFerr101 <pferreira59@gmail.com>
2018-04-05 15:34:07 +0100

    add toInclude.h

---
+++ include/toInclude.h
@@ -0,0 +1,29 @@
+#ifndef __TO_INCLUDE__
+#define __TO_INCLUDE__
+
+#include <gmodule.h>
+#include <glib.h>
+#include <stdlib.h>
+#include <libxml/parser.h>
+#include <string.h>
+#include <stdio.h>
+#include "interface.h"
+#include "common.h"
+#include "structs.h"
+#include "my_funcs.h"
+#include "load.h"
+#include "query_1.h"
+#include "query_2.h"
+#include "query_3.h"
+#include "query_4.h"
+#include "query_5.h"
+#include "query_6.h"
+#include "query_7.h"
+#include "query_8.h"
+#include "query_9.h"
+#include "query_10.h"
+#include "query_11.h"
+
+
+
+#endif
